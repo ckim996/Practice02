b41a67ce5611b137d890442688cf2cc542f3e085
Chanwoo Kim <chanwoo_kim96@hotmail.com>
2021-06-15 16:40:30 -0700

    Third Commit

--- src/Practice02.java
+++ src/Practice02.java
@@ -1,4 +1,12 @@
 public class Practice02 {
 
-    // Hi there, this should be second commit
+    // Hi there, this should be third commit
+    /*
+
+    For second commit, you have to Git -> Commit -> Call it your version x.x commit and then on the terminal
+    Put 'git push' - > Log on -> and it will push your updated commit version.
+
+    On github.com you can check the different versions of commit you did!
+
+     */
 }
